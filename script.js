06838614f3ffdac7ad34a414dbee6b55be7aa9b7
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-10-25 00:22:46 +0300

    comm

--- script.js
+++ script.js
@@ -3,20 +3,13 @@ let element = document.querySelector(`#element`);
 
 form.addEventListener(`submit`, evt => {
     evt.preventDefault();
-    evt.stopPropagation();
     const {target} = evt;
-    let formData = target.querySelectorAll(`input, select[name="shape"] option:checked`);
+    let formData = target.querySelectorAll(`#myColor, #shape`);
     let data = Array.from(formData).reduce((accumulator, item) => {
-        accumulator[item.name] = item.value;
-        // accumulator[item.textContent] = item.value;
-        // accumulator[item.type] = item.value;
+        accumulator[item.id] = item.value;
         return accumulator;
     }, {})
     element.removeAttribute('class');
-    element.classList.add(formData[0].label);
-    // element.classList.add(data.undefined);
+    element.classList.add(data.shape);
     element.style.backgroundColor = data.myColor;
-
-    console.log(data);
-    console.log(formData);
 });
